4d74e5509dd025eb531d8f2f2a60061928349349
CabaDev <cabadev@protonmail.com>
2019-04-03 22:44:37 -0500

    code 8 town parks end

--- Codigo/javascript/code8/script.js
+++ Codigo/javascript/code8/script.js
@@ -30,9 +30,9 @@ HINT: Use some of the ES6 features:
    arrow functions,
     destructuring, etc.
 */
-
+// Declaro 
 //Creamos una clase Global porque es lo que tenemos en comun entre las dos actividades. 
-
+//Clase Global
 class Global {
     constructor(name, yearBuild) {
         this.name = name;
@@ -43,26 +43,100 @@ class Global {
 //Luego creamos una clase extends para el parque
 class Parque extends Global {
     constructor(name, yearBuild, area, arboles) {
+            // super las que ya tenemos creadas y queremos heredar
+            super(name, yearBuild);
+            // declaramos las nuevas; 
+            this.area = area;
+            this.arboles = arboles;
 
-
-        //Nuevas variables 
-        super(area, arboles);
-        this.area = area;
-        this.arboles = arboles;
-
-    }
+        }
+        //Metodo()
     densidadxArboles() {
         const density = this.arboles / this.area;
         console.log(`${this.name} tiene una densidad de arboles de ${density} por kilometro cuadrado`);
 
-
-
-
     }
 
+}
+//Creamos una clase extends para calle 
+class Calle extends Global {
+    constructor(name, yearBuild, length, size = 3) {
+            // super las que ya tenemos creadas y queremos heredar
+            super(name, yearBuild);
+            // declaramos las nuevas; 
+            this.length = length;
+            this.size = size;
+        }
+        //Metodo()
+    clasificacionCalles() {
+        const classification = new Map();
+        classification.set(1, "tiny");
+        classification.set(2, "small");
+        classification.set(3, "normal");
+        classification.set(4, "big");
+        classification.set(5, "huge");
+        console.log(`${this.name}, build in ${this.yearBuild} y esta clasificado como ${classification.get(this.size)} calles`)
+    }
+
+}
+
+const todoslosParques = [new Parque("Green Park", 1987, 0.2, 215), new Parque("Nationa Park", 1894, 2.9, 3541), new Parque("Oak Park", 1953, 0.4, 949)];
+const todaslasCalles = [new Calle("Evergreen Street", 2008, 2.7, 2), new Calle("4th Street", 2015, 0.8, 3), new Calle("Sunset Boulevard", 1982, 2.5, 5)];
 
 
+function calc(arr) {
+
+    //Metodo reducir se usa para reducir un Array a un solo valor 
+
+
+    const sum = arr.reduce((prev, current, index) => prev + current, 0)
+        // Retorna la suma de la matriz y el promedio 
+    return [sum, sum / arr.length];
+
+}
+
+
+
+function reportParques(p) {
+
+    console.log("-- --PARQUE REPORT-- -- -- -- - ");
+    //Density
+    //Para cada elemento calculamos la densidad
+    p.forEach(element => {
+        element.densidadxArboles()
+
+    });
+
+    //Tomamos los años 
+    const ages = p.map(element => new Date().getFullYear() - element.yearBuild);
+    // Average Age
+    const [totalAge, avgAge] = calc(ages);
+    //Imprimimos 
+    console.log(`Our ${p.length} parks have an average of ${avgAge} years.`);
+
+    // Mora that 1000
+    const i = p.map(element => element.arboles).findIndex(el => el >= 1000);
+    console.log(`${p[i].name} has more than 1000 trees.`)
+}
+
+function reportCalles(s) {
+
+    // Total y promedio largo de las calles de el pueblo
+
+
+    const [totalLength, avgLength] = calc(s.map(element => element.length));
+    console.log(`Our ${s.length} streets have a total length of ${totalLength} km, with an average of ${avgLength} km.`);
+
+
+
+
+    // Classificar sizes 
+
+    s.forEach(element => element.clasificacionCalles());
 
 
 
 }
+//Llamo
+reportParques(todoslosParques);
+reportCalles(todaslasCalles);
