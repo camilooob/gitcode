9e8084e7bfb92c4d9ea92cd3234652c5ef0c1c5d
CabaDev <cabadev@protonmail.com>
2019-04-24 15:07:52 -0500

    primer reto node

--- Codigo/node/app.js
+++ Codigo/node/app.js
@@ -1,3 +1,11 @@
-const fs = require("fs");
+const fs = require("fs"); //Importamos la libreria fs 
 
 fs.writeFileSync("notes.txt", "Este archivo fue creado por Node.js");
+
+// Chellenge: Append a message to notes.txt
+//
+// 1. Use appendFileSync to append to the file
+// 2. Run the script 
+// 3. Check your work by opening the file and viewring the appended text
+
+fs.appendFileSync("notes.txt", " My name is Camilo and Im 25 years old");
