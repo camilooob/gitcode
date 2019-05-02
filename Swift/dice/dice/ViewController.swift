ac17df2755f4a694c2c6c2a1740ec6b328bfe54b
cabadev <cabadev@protonmail.com>
2019-05-02 15:16:09 -0500

    aprenndiendo ios
    proyecto dado ios

---
+++ Swift/dice/dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dice
+//
+//  Created by Camilo on 5/2/19.
+//  Copyright © 2019 cabadev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
