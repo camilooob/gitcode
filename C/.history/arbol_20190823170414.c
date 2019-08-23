0b31f83dac0e22543b99e78373c80328dd377449
Camilo <camilobaq@hotmail.com>
2019-08-23 17:54:35 -0500

    arbol c

---
+++ C/.history/arbol_20190823170414.c
@@ -0,0 +1,21 @@
+ #include <stdio.h>
+  #include<math.h>
+  #include <string.h>
+  #include <stdlib.h>
+ /* Funcion main inicia programa C*/
+ int main()
+ {
+int filas,numero=0,i=1;
+
+    printf("Digite el numero de filas del arbol: ")
+scanf("i%",&filas)
+// for(inicializacion; condicion; incremento) 
+for(i=1;i<=10;i++){
+if(i)
+
+//Codigo;
+
+}   
+     return 0;
+ }
+ /*Fin de la funciòn C */
---
+++ C/.history/arbol_20190823171818.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+/* Funcion main inicia programa C*/
+int main()
+{
+    int filas = 0, numero = 0, i = 1, num2 = 0;
+
+    printf("Digite el numero de filas del arbol: ")
+        scanf("i%", &filas)
+        // for(inicializacion; condicion; incremento)
+        for (i = 1; i <= filas; i++)
+    {
+        int num = 0;
+
+        while (num <= num2)
+        {
+            printf("*"); //Codigo while de 1 hasta 10
+            num++;       // i = i + 1; i+= 1;
+        }
+        num2++;
+        printf("\n")
+    }
+    return 0;
+}
+/*Fin de la funciòn C 
+    
