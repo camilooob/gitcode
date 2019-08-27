1b7a58106ad10ba6f73aa67b0249e824d8e3db54
Camilo <camilobaq@hotmail.com>
2019-08-27 14:18:18 -0500

    ascen c

---
+++ C/.history/ascendente_20190827141710.c
@@ -0,0 +1,12 @@
+ #include <stdio.h>
+  #include<math.h>
+  #include <string.h>
+  #include <stdlib.h>
+ /* Funcion main inicia programa C*/
+ int main()
+ {
+     int a,b,c;
+     
+     return 0;
+ }
+ /*Fin de la funciòn C */
