e28463006ac135f042ad3c9e432a545f7e51d919
Camilo <camilobaq@hotmail.com>
2019-08-28 16:57:37 -0500

    trigo

---
+++ C/.history/trigo_20190828150702.c
@@ -0,0 +1,8 @@
+/* Funciones Trigonomtricas: 
+
+acos(x) -> Calcular el arco de coseno de x.
+asin(x) -> Calcula el arco seno de x.
+atan(x) -> Calcula el arco tangente de x.
+cos(x)  -> calcula el coseno del angulo x, en radianes.
+sin(x)  -> Calcula el seno del angulo x, en radianes.
+tan(x)  -> Devuelve la tangente del angulo x, en radianes.
---
+++ C/.history/trigo_20190828164335.c
@@ -0,0 +1,33 @@
+/* Funciones Trigonomtricas: 
+
+acos(x) -> Calcular el arco de coseno de x.
+asin(x) -> Calcula el arco seno de x.
+atan(x) -> Calcula el arco tangente de x.
+cos(x)  -> calcula el coseno del angulo x, en radianes.
+sin(x)  -> Calcula el seno del angulo x, en radianes.
+tan(x)  -> Devuelve la tangente del angulo x, en radianes.
+*/
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+void funcion_trigonometrica();
+/* Funcion main inicia programa C*/
+int main()
+{
+    funcion_trigonometrica();
+
+    return 0;
+}
+/*Fin de la funciòn C */
+void funcion_trigonometrica()
+{
+    float x, cambio = 0;
+    printf("Digite un numero: \n");
+    printf("Entre -1 y 1");
+    scanf("%f", &x);
+
+    cambio = acos(x);
+    printf("%.2f", cambio);
+}
