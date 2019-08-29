0118455559e1ab2e9a08e4872c49bec071c28d51
Camilo <camilobaq@hotmail.com>
2019-08-28 21:00:06 -0500

    guardardon elementos array

---
+++ C/.history/array_20190828204142.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+/* Funcion main inicia programa C*/
+int main()
+{
+    int a[] = {1, 2, 3, 4, 5};
+    printf("%i", a[1]);
+
+    return 0;
+}
+/*Fin de la funciòn C */
---
+++ C/.history/guardar_20190828205350.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+/* Funcion main inicia programa C*/
+int main()
+{
+    float caja[10];
+    int i;
+    // for(inicializacion; condicion; incremento)
+    for (i = 0; i <= 10; i++)
+    {
+        printf("Abriendo Caja\n");
+        printf("Que elementos deseas guardar: ")
+            scanf("%f", &a[i]);
+
+        //Codigo;
+    }
+
+    return 0;
+}
+/*Fin de la funciòn C */
